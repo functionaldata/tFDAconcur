0aba7ffa5032295a739ebde6248bb9d3862660bc
iaosi <77047102+iaosi@users.noreply.github.com>
2023-09-19 23:13:31 -0700

    modify GetCI_Sparse

--- R/CI_sparse_FCReg.R
+++ R/CI_sparse_FCReg.R
@@ -4,7 +4,7 @@
 #' Each field corresponds to a functional (a list) or scalar (a vector) covariate. 
 #' The last entry is assumed to be the response if no entry is names 'Y'.
 #' If a field corresponds to a functional covariate, it should have two fields: 'Lt', a list of time points, and 'Ly', a list of function values.
-#' @param outGrid A vector with the output time points
+#' @param outGrid A vector with the output time points, which need to be within 5% and 95% of the range of functional covariates. If NULL, outGrid will be generated from 5% to 95% of the range of functional covariates with 51 grids for fitting. Default: NULL
 #' @param level A number taking values in [0,1] determing the confidence level. Default: 0.95.
 #' @param R An integer holding the number of bootstrap replicates. Default: 999.
 #' @param userBwMu A scalar bandwidth used for smoothing the mean function --- positive numeric - 
@@ -14,9 +14,9 @@
 #' @param kern Smoothing kernel choice, common for mu and covariance; 
 #' "rect", "gauss", "epan", "gausvar", "quar" (default: "gauss")
 #' @param measurementError Indicator measurement errors on the functional observations 
-#' should be assumed. If TRUE the diagonal raw covariance will be removed when smoothing. (default: TRUE)
+#' should be assumed. If TRUE the diagonal raw covariance will be removed when smoothing. (default: FALSE)
 #' @param diag1D  A string specifying whether to use 1D smoothing for the diagonal line of the covariance. 
-#' 'none': don't use 1D smoothing; 'cross': use 1D only for cross-covariances; 'all': use 1D for both auto- and cross-covariances. (default : 'none')
+#' 'none': don't use 1D smoothing; 'all': use 1D for both auto- and cross-covariances. (default : 'all')
 #' @param useGAM Indicator to use gam smoothing instead of local-linear smoothing (semi-parametric option) (default: FALSE)
 #' @param returnCov Indicator to return the covariance surfaces, which is a four dimensional array. The first two dimensions correspond to outGrid
 #'  and the last two correspond to the covariates and the response, i.e. (i, j, k, l) entry being Cov(X_k(t_i), X_l(t_j)) (default: FALSE)
@@ -80,8 +80,8 @@
 #'                              useGAM = FALSE, returnCov=TRUE)
 #' @export
 
-GetCI_Sparse = function(vars, outGrid, level = 0.95, R , userBwMu, userBwCov,  
-                        kern, measurementError, diag1D, useGAM, returnCov){
+GetCI_Sparse = function(vars, outGrid = NULL, level = 0.95, R = 999, userBwMu = NULL, userBwCov = NULL,  
+                        kern = "gauss", measurementError = FALSE, diag1D = "all", useGAM = FALSE, returnCov = FALSE){
   if (length(level) > 1) {
     level = level[1]
     warning("The input level has more than 1 element; only the first one is used.")
@@ -95,18 +95,70 @@ GetCI_Sparse = function(vars, outGrid, level = 0.95, R , userBwMu, userBwCov,
   
   n <- lengthVars(vars)
   p <- length(vars) - 1
+  
+  if (is.null(names(vars))){
+    names(vars) <- c(paste0('X', seq_len(length(vars) - 1)), 'Y')
+  }
+  
+  if ('Y' %in% names(vars)) {
+    vars <- c(vars[names(vars) != 'Y'], vars['Y'])
+    
+  } else if (names(vars)[length(vars)] == '') {
+    names(vars)[length(vars)] <- 'Y'
+  }
+  
+  Yname <- names(vars)[length(vars)]
+  
+  # Handle NaN, int to double
+  vars[sapply(vars, is.list)] <- lapply(
+    vars[sapply(vars, is.list)], 
+    function(v) HandleNumericsAndNAN(v[['Ly']], v[['Lt']])
+  )
+  # outGrid <- as.numeric(outGrid)
+  
+  # handle ourGrid range
+  temp <- lapply(
+    vars[sapply(vars, is.list)], 
+    function(v) {
+      return(range(unlist(v[['Lt']])))
+    }
+  )
+
+  l <- max(unlist(lapply(temp, function(v){return(v[1])})))
+  u <- min(unlist(lapply(temp, function(v){return(v[2])})))
+  
+  l5 = l+0.05*(u-l)
+  u95 = u-0.05*(u-l)
+  if(is.null(outGrid)){
+    outGrid = seq(l5, u95, length.out=51)
+  }else{
+    if(min(outGrid)<l5 | max(outGrid)>u95){
+      stop('The input "outGrid" is out of the 5% and 95% of the range of Lt, the list of time points in the "vars".')
+    }
+  }
+  
   betaMat <- lapply(1:R, function(b) {
-    ind <- sample(x = seq_len(n), size = n, replace = TRUE)
-    for(j in 1:(p+1)){
-      if ( is.list(vars[[j]]) ) {
-        vars[[j]]$Lt = vars[[j]]$Lt[ind]
-        vars[[j]]$Ly = vars[[j]]$Ly[ind]
-      }else{
-        vars[[j]] = vars[[j]][ind]
+    OutOfRange = TRUE
+    while (OutOfRange) {
+      ind = sample(x = seq_len(n), size = n, replace = TRUE)
+      temp_ind = lapply(vars[sapply(vars, is.list)], function(v) {return(range(unlist(v[['Lt']][ind])))})
+      l_ind = max(unlist(lapply(temp_ind, function(v){return(v[1])})))
+      u_ind = min(unlist(lapply(temp_ind, function(v){return(v[2])})))
+      if(l_ind <= l5 | u_ind >= u95){
+        OutOfRange = FALSE
       }
     }
-    #res <- ConcurReg(vars, outGrid, userBwMu = .5, userBwCov=.5,  kern='gauss', measurementError=TRUE, diag1D='none', useGAM = FALSE, returnCov=TRUE)
-    res <- ConcurReg(vars, outGrid, userBwMu = userBwMu, userBwCov = userBwCov,  kern = kern,
+    vars_ind = vars
+    for(j in 1:(p+1)){
+      if ( is.list(vars[[j]]) ) {
+        vars_ind[[j]]$Lt = vars[[j]]$Lt[ind]
+        vars_ind[[j]]$Ly = vars[[j]]$Ly[ind]
+      }else{
+        vars_ind[[j]] = vars[[j]][ind]
+      }
+    }
+    #res = ConcurReg(vars_ind, outGrid, userBwMu = .5, userBwCov=.5,  kern='gauss', measurementError=TRUE, diag1D='none', useGAM = FALSE, returnCov=TRUE)
+    res = ConcurReg(vars_ind, outGrid, userBwMu = userBwMu, userBwCov = userBwCov,  kern = kern,
            measurementError = measurementError, diag1D = diag1D, useGAM = useGAM , returnCov = returnCov)
     length(res$beta0)
     return(list(beta0 = res$beta0, beta = res$beta, R2 = res$R2, outGrid = res$outGrid))
